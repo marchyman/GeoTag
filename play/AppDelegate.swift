be5bab724be27139da87d394cf38bb3050ec8b27
Marco S Hyman <marc@snafu.org>
2014-06-11 21:10:26 -0700

    Initial Commit

---
+++ play/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  play
+//
+//  Created by Marco S Hyman on 6/11/14.
+//  Copyright (c) 2014 Marco S Hyman. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
