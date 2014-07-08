b3f5ba003b2b7e4697e9f6f90e7e172c16f56b66
Marco S Hyman <marc@snafu.org>
2014-07-08 00:28:55 -0700

    Changes for Xcode/swift beta 3

--- GeoTag/ImageData.swift
+++ GeoTag/ImageData.swift
@@ -30,62 +30,66 @@ class ImageData: NSObject {
         self.path = path;
         super.init()
         validImage = loadImageData()
+        // more init here
     }
 
     func loadImageData() -> Bool {
         /*
-         * Create an image source and grab the image properties
-         */
-        var imgRef = CGImageSourceCreateWithURL(path, nil).takeRetainedValue()
-        if imgRef == nil {
-            return false
+        * Create an image source and grab the image properties
+        */
+        if let unmanagedImageSource = CGImageSourceCreateWithURL(path, nil) {
+            let imgRef = unmanagedImageSource.takeRetainedValue()
+
+            // grab the image properties
+            let imgProps = CGImageSourceCopyPropertiesAtIndex(imgRef, 0, nil).takeUnretainedValue() as NSDictionary
+            println(imgProps)       // Debug
+            let height = imgProps["PixelHeight"] as Int!
+            let width = imgProps["PixelWidth"] as Int!
+
+            /*
+            * Create a "preview" of the image. If the image is larger than
+            * 512x512 constrain the preview to that size.  512x512 is an
+            * arbitrary limit.   Preview generation is used to work around a
+            * performance hit when using large raw images
+            */
+            var imgOpts: NSMutableDictionary = [
+                kCGImageSourceCreateThumbnailWithTransform : kCFBooleanTrue as AnyObject,
+                kCGImageSourceCreateThumbnailFromImageAlways : kCFBooleanTrue as AnyObject
+            ]
+            if height > 512 || width > 512 {
+                // add a max pixel size to the dictionary of options
+                imgOpts[kCGImageSourceThumbnailMaxPixelSize] = NSNumber.numberWithInt(512) as AnyObject
+            }
+            let imgPreview = CGImageSourceCreateThumbnailAtIndex (imgRef, 0, imgOpts).takeRetainedValue()
+
+            /*
+            * Create an NSImage from the preview
+            */
+            let imgHeight = Double(CGImageGetHeight(imgPreview))
+            let imgWidth = Double(CGImageGetWidth(imgPreview))
+            var imgRect = NSMakeRect(0.0, 0.0, imgHeight, imgWidth)
+            image = NSImage(size: imgRect.size)
+            // the following compiles, but the context is invalid
+            image.lockFocus()
+            CGContextDrawImage(NSGraphicsContext.currentContext().cgcontext, imgRect, imgPreview);
+            image.unlockFocus()
+            return true
         }
-        var imgProps = CGImageSourceCopyPropertiesAtIndex(imgRef, 0, nil).takeUnretainedValue() as NSDictionary
-        println(imgProps)       // Debug
-
-        /*
-         * Create a "thumbnail" of the image. If the image is larger than
-         * 512x512 constrain the thumbnail to that size.  512x512 is an
-         * arbitrary limit.   Thumbnail generation is used to work around a
-         * performance hit when using large raw images
-         */
-        var imgOpts: NSMutableDictionary =
-            [kCGImageSourceCreateThumbnailWithTransform : kCFBooleanTrue as AnyObject,
-            kCGImageSourceCreateThumbnailFromImageAlways : kCFBooleanTrue as AnyObject]
-        var height = imgProps["PixelHeight"] as Int!
-        var width = imgProps["PixelWidth"] as Int!
-        if height > 512 || width > 512 {
-            // add a max pixel size to the dictionary of options
-            imgOpts[kCGImageSourceThumbnailMaxPixelSize] = NSNumber.numberWithInt(512) as AnyObject
-        }
-        var imgPreview = CGImageSourceCreateThumbnailAtIndex (imgRef, 0, imgOpts).takeRetainedValue()
-
-        /*
-         * Create an NSImage from the preview
-         */
-        let imgHeight = Double(CGImageGetHeight(imgPreview))
-        let imgWidth = Double(CGImageGetWidth(imgPreview))
-        var imgRect = NSMakeRect(0.0, 0.0, imgHeight, imgWidth)
-        image = NSImage(size: imgRect.size)
-        let cgcontext = NSGraphicsContext.currentContext().cgcontext
-        image.lockFocus()
-        CGContextDrawImage(cgcontext, imgRect, imgPreview);
-        image.unlockFocus()
-
-        ///
-        return true
+        return false
     }
 }
 
+
 /*
- * extension found on stackoverflow to grab the graphics context.
- * fromOpaque turns a COpaquePointer to an Unmanaged<T>
+ * Convert the graphicsPort to a COpaquePointer
+ * fromOpaque turns the COpaquePointer to an Unmanaged<T>
  * takeUnretainedValue turns the Unmanaged<T> to an unretained T
  * It seems to work.
  */
 extension NSGraphicsContext {
     var cgcontext: CGContext {
-        return Unmanaged<CGContext>.fromOpaque(self.graphicsPort()).takeUnretainedValue()
+        let context = COpaquePointer(NSGraphicsContext.currentContext().graphicsPort())
+        return Unmanaged<CGContext>.fromOpaque(context).takeUnretainedValue()
     }
 }
 
