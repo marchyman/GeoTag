38da41a83d358aeabeca1e790a81a4259295adcc
Marco S Hyman <marc@snafu.org>
2014-07-08 00:30:04 -0700

    Empty playground

--- GeoTagPlay.playground/section-1.swift
+++ GeoTagPlay.playground/section-1.swift
@@ -4,10 +4,3 @@ import Cocoa
 
 var str = "Hello, playground"
 
-
-var opts: Dictionary<String, AnyObject> = [kCGImageSourceCreateThumbnailWithTransform : kCFBooleanTrue as AnyObject,
-    kCGImageSourceCreateThumbnailFromImageAlways : kCFBooleanTrue as AnyObject]
-
-opts[kCGImageSourceThumbnailMaxPixelSize] = NSNumber.numberWithInt(512)
-
-Void
