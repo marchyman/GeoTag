bf0ad0ae0b8335d1a464f460832fe5d3e80a47ef
Marco S Hyman <marc@snafu.org>
2014-07-08 17:26:38 -0700

    loadImageData seems to be working
    There is lots of test code in this commit regarding
    the extension to NSGraphicsContext to extract
    the current context.  I’m looking for a better way
    of doing this and expanded the code to see various
    values in the debugger and console.

--- GeoTag/ImageData.swift
+++ GeoTag/ImageData.swift
@@ -34,46 +34,36 @@ class ImageData: NSObject {
     }
 
     func loadImageData() -> Bool {
-        /*
-        * Create an image source and grab the image properties
-        */
-        if let unmanagedImageSource = CGImageSourceCreateWithURL(path, nil) {
-            let imgRef = unmanagedImageSource.takeRetainedValue()
-
-            // grab the image properties
+        if let imgRef = CGImageSourceCreateWithURL(path, nil)?.takeRetainedValue() {
+            /// grab the image properties
             let imgProps = CGImageSourceCopyPropertiesAtIndex(imgRef, 0, nil).takeUnretainedValue() as NSDictionary
-            println(imgProps)       // Debug
             let height = imgProps["PixelHeight"] as Int!
             let width = imgProps["PixelWidth"] as Int!
 
-            /*
-            * Create a "preview" of the image. If the image is larger than
-            * 512x512 constrain the preview to that size.  512x512 is an
-            * arbitrary limit.   Preview generation is used to work around a
-            * performance hit when using large raw images
-            */
+            /// Create a "preview" of the image. If the image is larger than
+            /// 512x512 constrain the preview to that size.  512x512 is an
+            /// arbitrary limit.   Preview generation is used to work around a
+            /// performance hit when using large raw images
+            let maxDimension = 512
             var imgOpts: NSMutableDictionary = [
                 kCGImageSourceCreateThumbnailWithTransform : kCFBooleanTrue as AnyObject,
                 kCGImageSourceCreateThumbnailFromImageAlways : kCFBooleanTrue as AnyObject
             ]
-            if height > 512 || width > 512 {
+            if height > maxDimension || width > maxDimension {
                 // add a max pixel size to the dictionary of options
-                imgOpts[kCGImageSourceThumbnailMaxPixelSize] = NSNumber.numberWithInt(512) as AnyObject
+                imgOpts[kCGImageSourceThumbnailMaxPixelSize] = maxDimension as AnyObject
             }
-            let imgPreview = CGImageSourceCreateThumbnailAtIndex (imgRef, 0, imgOpts).takeRetainedValue()
-
-            /*
-            * Create an NSImage from the preview
-            */
-            let imgHeight = Double(CGImageGetHeight(imgPreview))
-            let imgWidth = Double(CGImageGetWidth(imgPreview))
-            var imgRect = NSMakeRect(0.0, 0.0, imgHeight, imgWidth)
-            image = NSImage(size: imgRect.size)
-            // the following compiles, but the context is invalid
-            image.lockFocus()
-            CGContextDrawImage(NSGraphicsContext.currentContext().cgcontext, imgRect, imgPreview);
-            image.unlockFocus()
-            return true
+            if let imgPreview = CGImageSourceCreateThumbnailAtIndex(imgRef, 0, imgOpts)?.takeRetainedValue() {
+                /// Create an NSImage from the preview
+                let imgHeight = Double(CGImageGetHeight(imgPreview))
+                let imgWidth = Double(CGImageGetWidth(imgPreview))
+                var imgRect = NSMakeRect(0.0, 0.0, imgHeight, imgWidth)
+                image = NSImage(size: imgRect.size)
+                image.lockFocus()
+                CGContextDrawImage(NSGraphicsContext.currentContext().cgcontext, imgRect, imgPreview);
+                image.unlockFocus()
+                return true
+            }
         }
         return false
     }
@@ -88,8 +78,14 @@ class ImageData: NSObject {
  */
 extension NSGraphicsContext {
     var cgcontext: CGContext {
-        let context = COpaquePointer(NSGraphicsContext.currentContext().graphicsPort())
-        return Unmanaged<CGContext>.fromOpaque(context).takeUnretainedValue()
+        let graphicsPort = NSGraphicsContext.currentContext().graphicsPort()
+        let context = COpaquePointer(graphicsPort)
+        println("context \(context)")
+        let uValue = Unmanaged<CGContext>.fromOpaque(context)
+        println("uValue \(uValue)")
+        let value = uValue.takeUnretainedValue()
+        println("value \(value)")
+        return value
     }
 }
 
