0d1d6852eda0f7775b87137ea953dfa08eaabce6
Marco S Hyman <marc@snafu.org>
2014-07-15 22:37:11 -0700

    Display fields in table view
    Debug code populates table with two entries for now.

--- GeoTag/TableViewController.swift
+++ GeoTag/TableViewController.swift
@@ -12,44 +12,53 @@ import Cocoa
 class TableViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
 
     @IBOutlet var tableView: NSTableView
-    
+    var images: [ImageData] = []
+
     // startup
 
-    override func viewDidLoad() {
+    override func viewDidLoad() {       // 10.10 and later
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func addImageNamed(name: String) {
+        images.append(ImageData(path: NSURL(string: name)))
     }
 
     // data source functions
     
     func numberOfRowsInTableView(tableView: NSTableView!) -> Int {
+        addImageNamed("file:///Users/marc/Desktop/p-141711415-1432.jpg")
+        addImageNamed("file:///Users/marc/Desktop/p-141660942-1403.raf")
         // Number of active rows
-        println(__FUNCTION__ + ": for table view \(tableView)")
-        return 1
+        println("\(images.count) active rows")
+        println("arg: \(tableView) outlet: \(self.tableView)")
+        return images.count
     }
 
     func tableView(tableView: NSTableView!,
         viewForTableColumn tableColumn: NSTableColumn!,
         row: Int) -> NSView! {
+        let image = images[row]
+        var value = ""
         if let id = tableColumn.identifier {
             switch id {
-                case "imageName":
-                     println("imageName")
-
-                case "dateTime":
-                    println("dateTime")
-
-                case "latitude":
-                    println("latitude")
-                
-                case "longitude":
-                    println("longitude")
-
+            case "imageName":
+                value = image.name
+            case "dateTime":
+                value = image.date
+            case "latitude":
+                if let lat = image.latitude {
+                    value = NSString(format: "% 2.6f", lat)
+                }
+            case "longitude":
+                if let lon = image.longitude {
+                    value = NSString(format: "% 2.6f", lon)
+                }
             default:
-                    println("unknown id")
+                break
             }
             var colView = tableView.makeViewWithIdentifier(id, owner: nil) as NSTableCellView
-            colView.textField.stringValue = "Test " + id;    // test code
+            colView.textField.stringValue = value;
             return colView
         }
         return nil
