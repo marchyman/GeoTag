972314fade753a10ff7b682b4339c8ed93b1c700
Marco S Hyman <marc@snafu.org>
2015-03-09 14:27:43 -0700

    playground changes

--- GeoTagPlay.playground/section-1.swift
+++ GeoTagPlay.playground/section-1.swift
@@ -4,12 +4,43 @@ import Cocoa
 
 var str = "Hello, playground"
 
-class Foo {
-    var variable: Int = 42
+
+/* The following works here but crashes when I change the AppDelegate
+   code.  Will look at the crash, later */
+
+   // storage for stored class variable
+struct Statics {
+    static var staticFoo: String!
 }
 
-var foo: Foo? = Foo()
-
-if let bar = foo?.variable {
-    println("foo.variable is \(bar)")
+class SomeClass {
+    // stored class variables not supported, do this trick
+    class var staticFoo: String! {
+        get {
+            return Statics.staticFoo
+        }
+        set {
+            Statics.staticFoo = newValue
+        }
+    }
+    init(foo: String) {
+        SomeClass.staticFoo = foo
+    }
 }
+
+var someClass = SomeClass(foo: "static foo value")
+SomeClass.staticFoo
+
+// -------------
+
+class AnotherClass {
+    static var staticBar: String!
+
+    init(bar: String) {
+        AnotherClass.staticBar = bar
+    }
+
+}
+
+var anotherClass = AnotherClass(bar: "static bar value")
+AnotherClass.staticBar
