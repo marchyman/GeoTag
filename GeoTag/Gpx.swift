83b3462b2c077d5ef868e7058ea0bda7f95a0d3e
Marco S Hyman <marc@snafu.org>
2019-08-19 18:29:40 -0700

    Fix possible off-by-one
    When searching for a trackpoint matching the timestamp of an image accept an exact match.  The function was returning the previous point if an exact match happened to occur.
    
    Also handle non ISO 8601 dates when processing track logs

--- GeoTag/Gpx.swift
+++ GeoTag/Gpx.swift
@@ -54,6 +54,7 @@ class Gpx: NSObject {
 
     /// date formater for track point timestamps.
     static let pointTimeFormat = ISO8601DateFormatter()
+    static let altTimeFormat = DateFormatter()
 
     /// Track points contain (at least) a latitude, longitude, and timestamp
     struct Point {
@@ -65,6 +66,12 @@ class Gpx: NSObject {
                                                         options: .regularExpression)
             if let convertedTime = pointTimeFormat.date(from: trimmedTime) {
                 return convertedTime.timeIntervalSince1970
+            }
+            // the time is not in ISO8601 format. Assume it is in local time.
+            altTimeFormat.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+            altTimeFormat.timeZone = nil
+            if let altConvertedTime = altTimeFormat.date(from: trimmedTime) {
+                return altConvertedTime.timeIntervalSince1970
             }
             return 0
         }
@@ -162,7 +169,7 @@ class Gpx: NSObject {
             track in
             for segment in track.segments {
                 let possiblePoints = segment.points.prefix {
-                    $0.timeFromEpoch < imageTime
+                    $0.timeFromEpoch <= imageTime
                 }
                 if let segmentLast = possiblePoints.last {
                     if lastPoint == nil {
