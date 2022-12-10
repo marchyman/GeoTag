8d9ae66ddefb77b4109eb37d62f836ff0c01fdcb
Marco S Hyman <marc@snafu.org>
2022-12-09 22:16:04 -0800

    Housekeeping, Icons, Copyright

---
+++ GeoTag/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GeoTag
+//
+//  Created by Marco S Hyman on 12/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeoTag/GeoTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoTagApp.swift
+//  GeoTag
+//
+//  Created by Marco S Hyman on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GeoTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
