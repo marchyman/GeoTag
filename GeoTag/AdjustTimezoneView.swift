43a1641fecd3666fe539aa71f6187c6f85e521e9
Marco S Hyman <marc@snafu.org>
2023-01-04 22:07:51 -0800

    Add window to select alternate time zone
    Used when matching track logs and the timezone of the images do not match the timezone of the computer.

--- GeoTag/AdjustTimezoneView.swift
+++ GeoTag/AdjustTimezoneView.swift
@@ -9,34 +9,66 @@ import SwiftUI
 
 struct AdjustTimezoneView: View {
     @EnvironmentObject var vm: ViewModel
+    @State private var currentZone: TimeZoneName = .zero
+    @State private var selectedZone: TimeZoneName = .zero
 
     var body: some View {
         VStack {
             Text("Specify Camera Time Zone")
                 .font(.largeTitle)
-                .padding()
             Text("""
-                 When matching images to a GPS track log GeoTag assumes image
-                 Time Zones are the local-to-this-computer Time Zone. If the
-                 camera was set to the local time in some other time zone
-                 select the appropriate time zone here.
+                 When matching images to a GPS track log GeoTag assumes the
+                 Time Zone of image timestamps to be the same as the
+                 local-to-this-computer Time Zone. If the camera was set to
+                 the local time in some other Time Zone or the Time Zone has
+                 changed since you took the pictures select the appropriate
+                 Time Zone here before matching image timestamps to a track
+                 log.
                  """)
                 .padding()
+
             Divider()
-            Text("Current Camera Time Zone")
-                .padding()
+
+            Form {
+                LabeledContent("Current Camera Time Zone:") {
+                    VStack (alignment: .leading){
+                        Text(currentZone.rawValue)
+                        Text(currentZone.timeZone.identifier)
+                    }
+                }
+                .padding(.bottom)
+
+                LabeledContent("Desired Camera Time Zone:") {
+                    VStack( alignment: .leading) {
+                        Picker("Desired Camera Time Zone",
+                               selection: $selectedZone) {
+                            ForEach (TimeZoneName.allCases) { zone in
+                                Text(zone.rawValue)
+
+                            }
+                        }
+                        .labelsHidden()
+                        .frame(width: 100)
+                        Text(selectedZone.timeZone.identifier)
+                    }
+                }
+
+            }
+            .padding(30)
+
             Divider()
-            Text("New Camera Time Zone")
-                .padding()
-            Divider()
+
             HStack {
                 Spacer()
                 Button("Cancel") {
                     NSApplication.shared.keyWindow?.close()
                 }
                 .keyboardShortcut(.cancelAction)
+
                 Button("Change") {
-                    // update ViewModel here
+                    if currentZone != selectedZone {
+                        vm.timeZone = selectedZone.timeZone
+                    }
                     NSApplication.shared.keyWindow?.close()
                 }
                 .keyboardShortcut(.defaultAction)
@@ -44,7 +76,11 @@ struct AdjustTimezoneView: View {
             .padding()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .frame(width: 500.0, height: 400.0)
+        .frame(width: 600.0, height: 500.0)
+        .onAppear {
+            currentZone = TimeZoneName.timeZoneCase(zone: vm.timeZone)
+            selectedZone = currentZone
+        }
     }
 }
 
