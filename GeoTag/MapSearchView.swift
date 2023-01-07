f52d96c315936dbc1b310b1b104625e8ee36101b
Marco S Hyman <marc@snafu.org>
2023-01-06 19:30:53 -0800

    Map Search Bar Menu
    I realized the source of the error that caused me to code an unneeded workaround.  Fix the code and get rid of the workaround.

--- GeoTag/MapSearchView.swift
+++ GeoTag/MapSearchView.swift
@@ -31,43 +31,41 @@ struct MapSearchView: NSViewRepresentable {
     }
 
     func makeNSView(context: Context) -> NSSearchField {
-        let searchField = NSSearchField.searchFieldWithMenu()
+        let searchField = NSSearchField(frame: .zero)
         searchField.delegate = context.coordinator
+        searchField.searchMenuTemplate = makeSearchesMenu()
         return searchField
     }
 
     func updateNSView(_ nsView: NSSearchField, context: Context) {
         nsView.stringValue = text
     }
+
+    func makeSearchesMenu() -> NSMenu {
+        let searchesMenu: NSMenu = NSMenu(title: "Recents")
+
+        let i1 = searchesMenu.addItem(withTitle: "Recents Search",
+                                      action: nil, keyEquivalent: "")
+        i1.tag = Int(NSSearchField.recentsTitleMenuItemTag)
+
+        let i2 = searchesMenu.addItem(withTitle: "Item",
+                                      action: nil, keyEquivalent: "")
+        i2.tag = Int(NSSearchField.recentsMenuItemTag)
+
+        let i3 = searchesMenu.addItem(withTitle: "Clear",
+                                      action: nil, keyEquivalent: "")
+        i3.tag = Int(NSSearchField.clearRecentsMenuItemTag)
+
+        let i4 = searchesMenu.addItem(withTitle: "No Recent Search",
+                                      action: nil, keyEquivalent: "")
+        i4.tag = Int(NSSearchField.noRecentsMenuItemTag)
+
+        return searchesMenu
+    }
 }
 
 struct MapSearchView_Previews: PreviewProvider {
     static var previews: some View {
         MapSearchView(text: .constant(""))
-    }
-}
-
-// Can not update searchMenuTemplate in makeNSView.  Instead create a static
-// function for NSSearchField that creates a view tht makeNSView can use.
-
-extension NSSearchField {
-
-    static func searchFieldWithMenu() -> NSSearchField {
-        let searchesMenu: NSMenu = {
-            let menu = NSMenu(title: "Recents")
-            let i1 = menu.addItem(withTitle: "Recents Search", action: nil, keyEquivalent: "")
-            i1.tag = Int(NSSearchField.recentsTitleMenuItemTag)
-            let i2 = menu.addItem(withTitle: "Item", action: nil, keyEquivalent: "")
-            i2.tag = Int(NSSearchField.recentsMenuItemTag)
-            let i3 = menu.addItem(withTitle: "Clear", action: nil, keyEquivalent: "")
-            i3.tag = Int(NSSearchField.clearRecentsMenuItemTag)
-            let i4 = menu.addItem(withTitle: "No Recent Search", action: nil, keyEquivalent: "")
-            i4.tag = Int(NSSearchField.noRecentsMenuItemTag)
-            return menu
-        }()
-
-        let searchField = NSSearchField(frame: .zero)
-        searchField.searchMenuTemplate = searchesMenu
-        return searchField
     }
 }
