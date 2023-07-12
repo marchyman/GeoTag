9a9c1727c9702bf01259e5196be638f5779dc60e
Marco S Hyman <marc@snafu.org>
2023-07-12 13:48:50 -0700

    Lat/Lon input in inspector

--- GeoTag/LatLonSectionView.swift
+++ GeoTag/LatLonSectionView.swift
@@ -6,15 +6,94 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct LatLonSectionView: View {
-    @Bindable var image: ImageModel
+    var image: ImageModel
     @Environment(AppState.self) var state
     @FocusState private var isFocused: Bool
 
+    @State private var latitude: Double?
+    @State private var longitude: Double?
+
     var body: some View {
-        Text("Latitude adjustments")
-        Text("Longitude adjustments")
+        VStack {
+            LabeledContent("Latitude:") {
+                TextField("Latitude", value: $latitude, format: .latitude)
+                    .frame(width: 200)
+                    .labelsHidden()
+                    .padding()
+                    .focused($isFocused)
+                    .focusedValue(\.textfieldBinding, $latitude)
+            }
+
+            LabeledContent("Longitude:") {
+                TextField("Longitude", value: $longitude, format: .longitude)
+                    .frame(width: 200)
+                    .labelsHidden()
+                    .padding()
+                    .focused($isFocused)
+                    .focusedValue(\.textfieldBinding, $longitude)
+            }
+        }
+        .textFieldStyle(.roundedBorder)
+        .onExitCommand {
+            loadCoordinates()
+            isFocused = false
+        }
+        .onSubmit {
+            if validateLocation() {
+                updateLocation()
+                isFocused = false
+            }
+        }
+        .onChange(of: image.location) {
+            loadCoordinates()
+        }
+    }
+
+    // set state variables from image location
+
+    private func loadCoordinates() {
+        if let location = image.location {
+            latitude = location.latitude
+            longitude = location.longitude
+        } else {
+            latitude = nil
+            longitude = nil
+        }
+    }
+
+    // check that latitude and longitude are non-nil and in appropriate range
+
+    private func validateLocation() -> Bool {
+        if let latitude,
+           let longitude,
+           (0...90).contains(latitude.magnitude),
+           (0...180).contains(longitude.magnitude) {
+            return true
+        }
+        return false
+    }
+
+    // update all selected images with the location specified by the
+    // state variables.
+
+    private func updateLocation() {
+        // function won't be called if lat/lon are nil.
+        // guard used to convert to non-optional values
+        guard let latitude, let longitude else { return }
+        var newLocation = Coords(latitude: latitude,
+                                 longitude: longitude)
+
+        state.undoManager.beginUndoGrouping()
+        for id in state.tvm.selection where state.tvm[id].isValid {
+            if state.tvm[id].location != newLocation {
+                state.update(id: id, location: newLocation)
+            }
+        }
+        state.undoManager.endUndoGrouping()
+        state.undoManager.setActionName("modify location")
     }
 }
 
