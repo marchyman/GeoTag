cdacc2b03baf90fbe0a827758013ba12087ac62d
Marco S Hyman <marc@snafu.org>
2024-03-27 20:19:13 -0700

    GeoTag UI tests
    Verify the elements of the user interface are present. Measure launch
    time.

--- GeoTagUITests/GeoTagUITests.swift
+++ GeoTagUITests/GeoTagUITests.swift
@@ -9,33 +9,147 @@ import XCTest
 
 final class GeoTagUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private var app: XCUIApplication!
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    override func setUpWithError() throws {
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launchEnvironment = ["UITESTS": "1"]
+        app.launch()
+
+        // Now set up user defaults for testing.  The app should be showing
+        // a sheet saying no backup file exists.  Dismiss the sheet.
+
+        let sheet = app.windows.sheets.element
+        XCTAssertTrue(sheet.exists)
+        takeScreenshot(name: "InitialLaunch")
+        sheet.buttons.firstMatch.click()
+        takeScreenshot(name: "Launch")
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // Put teardown code here. This method is called after the
+        // invocation of each test method in the class.
+        try super.tearDownWithError()
+        app = nil
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+    func takeScreenshot(name: String) {
+        let screenshot = app.windows.firstMatch.screenshot()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let attachment =
+            XCTAttachment(uniformTypeIdentifier: "public.png",
+                          name: "\(name).png",
+                          payload: screenshot.pngRepresentation,
+                          userInfo: nil)
+        attachment.lifetime = .keepAlways
+        add(attachment)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    // test that app contains the normal elements at initial launch and that
+    // the appropriate menu items appear.
+    func test0Startup() {
+        let window = app.windows["main"]
+        XCTAssertTrue(window.exists)
+        XCTAssertEqual(window.descendants(matching: .tableColumn).count, 4)
+        XCTAssertEqual(window.descendants(matching: .image).count, 2)
+        XCTAssertTrue(window.descendants(matching: .map).element.exists)
+        XCTAssertTrue(window.descendants(matching: .toolbar).element.exists)
+        XCTAssertTrue(app.buttons["Toggle Inspector"].exists)
+        app.buttons["Toggle Inspector"].firstMatch.click()
+        XCTAssertTrue(app.staticTexts["Please select an image"].waitForExistence(timeout: 2))
+        takeScreenshot(name: "Inspector")
+        app.buttons["Toggle Inspector"].firstMatch.click()
+
+        let menubar = app.menuBars.element
+        XCTAssertTrue(menubar.exists)
+        XCTAssertEqual(menubar.children(matching: .menuBarItem).count, 7)
+        menuBarItem0(menubar.children(matching: .menuBarItem).element(boundBy: 0))
+        menuBarItem1(menubar.children(matching: .menuBarItem).element(boundBy: 1))
+        menuBarItem2(menubar.children(matching: .menuBarItem).element(boundBy: 2))
+        menuBarItem3(menubar.children(matching: .menuBarItem).element(boundBy: 3))
+        menuBarItem4(menubar.children(matching: .menuBarItem).element(boundBy: 4))
+        menuBarItem5(menubar.children(matching: .menuBarItem).element(boundBy: 5))
+        menuBarItem6(menubar.children(matching: .menuBarItem).element(boundBy: 6))
     }
+
+    // set the "no backup" flag in Settings to get rid of the warning sheet
+    // when the app is launched in future tests.
+    func test1SetNoBackup() {
+        app.menuItems["Settings…"].click()
+        print(app.debugDescription)
+        XCTAssertTrue(app.windows["GeoTag Settings"].waitForExistence(timeout: 2))
+        app.checkBoxes["Disable image backups"].click()
+        app.buttons["Close"].click()
+    }
+
+    // Apple menu
+    func menuBarItem0(_ item: XCUIElement) {
+        XCTAssertTrue(item.exists)
+    }
+
+    // GeoTag menu
+    func menuBarItem1(_ item: XCUIElement) {
+        XCTAssertTrue(item.exists)
+        XCTAssertEqual(item.title, "GeoTag")
+        XCTAssertTrue(item.descendants(matching: .menuItem)["About GeoTag"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Settings…"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Quit GeoTag"].exists)
+    }
+
+    // File menu
+    func menuBarItem2(_ item: XCUIElement) {
+        XCTAssertTrue(item.exists)
+        XCTAssertEqual(item.title, "File")
+        XCTAssertGreaterThan(item.descendants(matching: .menuItem).count, 6)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Open…"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Close"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Save…"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Discard changes"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Discard tracks"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Clear Image List"].exists)
+    }
+
+    // Edit menu
+    func menuBarItem3(_ item: XCUIElement) {
+        XCTAssertTrue(item.exists)
+        XCTAssertEqual(item.title, "Edit")
+        XCTAssertGreaterThan(item.descendants(matching: .menuItem).count, 10)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Undo"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Redo"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Cut"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Copy"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Paste"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Delete"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Select All"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Show In Finder"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Locn From Track"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Specify Time Zone…"].exists)
+    }
+
+    // View menu
+    func menuBarItem4(_ item: XCUIElement) {
+        XCTAssertTrue(item.exists)
+        XCTAssertEqual(item.title, "View")
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Hide Disabled Files"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Pin view options…"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Show pins for all selected items"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Show pin for most selected item"].exists)
+    }
+
+    // Window menu
+    func menuBarItem5(_ item: XCUIElement) {
+        XCTAssertTrue(item.exists)
+        XCTAssertEqual(item.title, "Window")
+    }
+
+    // Help menu
+    func menuBarItem6(_ item: XCUIElement) {
+        XCTAssertTrue(item.exists)
+        XCTAssertEqual(item.title, "Help")
+        XCTAssertTrue(item.descendants(matching: .menuItem)["GeoTag 5 Help…"].exists)
+        XCTAssertTrue(item.descendants(matching: .menuItem)["Report a bug…"].exists)
+    }
+
 }
