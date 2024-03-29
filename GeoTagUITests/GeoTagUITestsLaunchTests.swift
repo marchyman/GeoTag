34a134419fbefb57f95be971e361dfbf8e13b345
Marco S Hyman <marc@snafu.org>
2024-03-28 20:21:16 -0700

    More UI tests
    An ongoing work in progress.

--- GeoTagUITests/GeoTagUITestsLaunchTests.swift
+++ GeoTagUITests/GeoTagUITestsLaunchTests.swift
@@ -18,6 +18,13 @@ final class GeoTagUITestsLaunchTests: XCTestCase {
         app.launch()
     }
 
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the
+        // invocation of each test method in the class.
+        try super.tearDownWithError()
+        app = nil
+    }
+
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
