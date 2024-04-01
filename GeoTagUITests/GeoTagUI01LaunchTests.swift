90e803dc984badf32c9b195fd71b81bd82b94c07
Marco S Hyman <marc@snafu.org>
2024-04-01 13:07:22 -0700

    enable code coverage
    Also remove unneeded empty test.

--- GeoTagUITests/GeoTagUI01LaunchTests.swift
+++ GeoTagUITests/GeoTagUI01LaunchTests.swift
@@ -27,11 +27,6 @@ final class GeoTagUI01LaunchTests: XCTestCase {
         app = nil
     }
 
-    func testSetBackup() {
-        // do nothing.  A side effect of running the test will set up
-        // a backup folder.
-    }
-
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
