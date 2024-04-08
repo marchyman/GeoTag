b668609cb93cc996fce42439ad23337895984730
Marco S Hyman <marc@snafu.org>
2024-04-07 17:58:15 -0700

    More testing

--- GeoTagUITests/GeoTagUI05Tests.swift
+++ GeoTagUITests/GeoTagUI05Tests.swift
@@ -47,6 +47,20 @@ final class GeoTagUI05Tests: XCTestCase {
         app.typeKey(.enter, modifierFlags: [])
     }
 
+    func changeCoordFormat(_ fmt: Int) {
+        let format: [String] = [
+            "dd.dddddd",
+            "dd° mm.mmmmmm'",
+            "dd° mm' ss.ss\""
+        ]
+        XCTAssert(fmt >= 0 && fmt <= 2)
+        app.typeKey(",", modifierFlags: .command)
+        let settings = app.windows["GeoTag Settings"]
+        XCTAssert(settings.waitForExistence(timeout: 2))
+        settings.radioButtons[format[fmt]].click()
+        settings.buttons["Close"].click()
+    }
+
     func test0DuplicateImage() {
         openTestFile()
         XCTAssertTrue(app.staticTexts["IMG_7158.CR2*"]
@@ -114,5 +128,80 @@ final class GeoTagUI05Tests: XCTestCase {
 //                          .waitForExistence(timeout: 1))
 //        XCTAssertFalse(row.staticTexts.element(boundBy: 3).exists)
 //        XCTAssertFalse(map.images["Pin"].exists)
+    }
+
+    func test2TableMenu() {
+        openTestFile(folder: true)
+        XCTAssertTrue(app.windows.sheets.element.waitForExistence(timeout: 2))
+        app.sheets.buttons.firstMatch.click()
+
+        // Check context menu when right clicking on table
+        let table = app.outlines.firstMatch
+        table.rightClick()
+        let menu = app.windows.firstMatch
+                    .groups.firstMatch
+                    .splitGroups.firstMatch
+                    .groups.firstMatch
+                    .menus.firstMatch
+        XCTAssert(menu.waitForExistence(timeout: 1))
+        XCTAssert(menu.menuItems["Edit…"].exists)
+        XCTAssert(menu.menuItems["Cut"].exists)
+        XCTAssert(menu.menuItems["Copy"].exists)
+        XCTAssert(menu.menuItems["Paste"].exists)
+        XCTAssert(menu.menuItems["Delete"].exists)
+        XCTAssert(menu.menuItems["Show In Finder"].exists)
+        XCTAssert(menu.menuItems["Locn From Track"].exists)
+        XCTAssert(menu.menuItems["Clear Image List"].exists)
+        table.click()
+
+        // assign locations and test the various coordinate formats
+
+        changeCoordFormat(0)
+        app.typeKey("a", modifierFlags: [.command])
+        table.rightClick()
+        menu.menuItems["Locn From Track"].click()
+        var row = app.outlineRows.firstMatch
+        if !row.staticTexts["IMG_7158.CR2*"].exists {
+            row = app.outlineRows.element(boundBy: 1)
+        }
+        XCTAssert(row.staticTexts[" 38.521077"].waitForExistence(timeout: 1))
+        XCTAssert(row.staticTexts["-123.200344"].exists)
+
+        changeCoordFormat(1)
+        XCTAssert(row.staticTexts["38° 31.264616' N"].waitForExistence(timeout: 1))
+        XCTAssert(row.staticTexts["123° 12.020668' W"].exists)
+
+        changeCoordFormat(2)
+        XCTAssert(row.staticTexts["38° 31' 15.88\" N"].waitForExistence(timeout: 1))
+        XCTAssert(row.staticTexts["123° 12' 1.24\" W"].exists)
+
+        // copy the coordinates and paste them into another row
+        row.staticTexts.element(boundBy: 0).rightClick()
+        menu.menuItems["Copy"].click()
+        let newRow = app.outlineRows.element(boundBy: 4)
+        newRow.staticTexts.element(boundBy: 0).click()
+        newRow.staticTexts.element(boundBy: 0).rightClick()
+        menu.menuItems["Paste"].click()
+        XCTAssert(newRow.staticTexts["38° 31' 15.88\" N"].exists)
+        XCTAssert(newRow.staticTexts["123° 12' 1.24\" W"].exists)
+
+        // open the inspector for newRow
+        newRow.staticTexts.element(boundBy: 0).rightClick()
+        menu.menuItems["Edit…"].click()
+        let inspector = app
+            .windows.firstMatch
+            .groups.firstMatch
+            .splitGroups.firstMatch
+            .groups.element(boundBy: 1)
+        print(inspector.debugDescription)
+        XCTAssert(inspector.staticTexts["Date and Time"].exists)
+        XCTAssert(inspector.staticTexts["Location"].exists)
+        let lat = inspector.textFields.firstMatch
+        lat.click()
+        lat.typeKey("a", modifierFlags: [.command])
+        lat.typeText("38.5")
+        lat.typeKey(.return, modifierFlags: [])
+        app.buttons["Toggle Inspector"].firstMatch.click()
+        XCTAssert(newRow.staticTexts["38° 30' 0.00\" N"].exists)
     }
 }
