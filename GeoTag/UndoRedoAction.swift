774b957725bb8e0b88a18013749409efd9aa5209
Marco S Hyman <marc@snafu.org>
2024-04-15 19:56:43 -0700

    Move undo/redo disabled check override
    Move the override to the enabled/disabled check so I can log the current
    status and maybe notice when (if) it starts working again.

--- GeoTag/UndoRedoAction.swift
+++ GeoTag/UndoRedoAction.swift
@@ -7,18 +7,27 @@
 
 import Foundation
 
+// macOS 14.3 and later: undo and redo disabled checks are only done when the
+// the app is launched.  As a result undo/redu are always disabled.  Comment
+// the reuturn of the real value and return false (never disabled).  Live
+// with menu item titles that do not change
+
 extension AppState {
 
     // return true if the undo menu option should be disabled.
 
     var undoDisabled: Bool {
-        return !undoManager.canUndo
+        // swiftlint: disable line_length
+        Self.logger.notice("canUndo: \(self.undoManager.canUndo, privacy: .public), \(self.undoManager.levelsOfUndo, privacy: .public) levels")
+        // swiftlint: enable line_length
+        return false // !undoManager.canUndo
     }
 
     // return true if the redo menu option should be disabled.
 
     var redoDisabled: Bool {
-        return !undoManager.canRedo
+        Self.logger.notice("canRedo: \(self.undoManager.canRedo, privacy: .public)")
+        return false // !undoManager.canRedo
     }
 
     func undoAction() {
--- GeoTag/UndoRedoCommandGroup.swift
+++ GeoTag/UndoRedoCommandGroup.swift
@@ -12,19 +12,15 @@ import SwiftUI
 struct UndoRedoCommands: Commands {
     var state: AppState
 
-    // macOS 14.3 and later: state undo values are never checked.  As a result
-    // undo/redu are always disabled.  Comment out the check and live with
-    // menu item titles that do not change
-
     var body: some Commands {
         CommandGroup(replacing: .undoRedo) {
             Button(state.undoManager.undoMenuItemTitle) { state.undoAction() }
                 .keyboardShortcut("z")
-                // .disabled(state.undoDisabled)
+                 .disabled(state.undoDisabled)
 
             Button(state.undoManager.redoMenuItemTitle) { state.redoAction() }
                 .keyboardShortcut("z", modifiers: [.shift, .command])
-                // .disabled(state.redoDisabled)
+                 .disabled(state.redoDisabled)
         }
     }
 }
