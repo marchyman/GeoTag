df7dde58c199eed9a50282bf9bd971b5b576a5ef
Marco S Hyman <marc@snafu.org>
2024-07-11 14:30:26 -0700

    Project builds with Split[HV]View packages

--- SplitHView/Package.swift
+++ SplitHView/Package.swift
@@ -5,19 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "SplitHView",
+    platforms: [.macOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SplitHView",
-            targets: ["SplitHView"]),
+        .library(name: "SplitHView", targets: ["SplitHView"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SplitHView"),
-        .testTarget(
-            name: "SplitHViewTests",
-            dependencies: ["SplitHView"]),
+        .target(name: "SplitHView"),
+        .testTarget(name: "SplitHViewTests", dependencies: ["SplitHView"])
     ]
 )
--- SplitVView/Package.swift
+++ SplitVView/Package.swift
@@ -5,19 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "SplitVView",
+    platforms: [.macOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SplitVView",
-            targets: ["SplitVView"]),
+        .library(name: "SplitVView", targets: ["SplitVView"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SplitVView"),
-        .testTarget(
-            name: "SplitVViewTests",
-            dependencies: ["SplitVView"]),
+        .target(name: "SplitVView"),
+        .testTarget(name: "SplitVViewTests", dependencies: ["SplitVView"])
     ]
 )
