4d97f78af9a35b8205d4a12618ac5d128ccb9cc5
Marco S Hyman <marc@snafu.org>
2024-07-11 14:18:03 -0700

    Separate SplitViews
    Make SplitHView and SplitVView separate packages

--- SplitHView/Package.swift
+++ SplitHView/Package.swift
@@ -1,21 +1,23 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SplitViews",
-    platforms: [.macOS(.v14)],
+    name: "SplitHView",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SplitViews",
-            targets: ["SplitHView", "SplitVView"]),
+            name: "SplitHView",
+            targets: ["SplitHView"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "SplitHView"),
-        .target(name: "SplitVView"),
+        .target(
+            name: "SplitHView"),
+        .testTarget(
+            name: "SplitHViewTests",
+            dependencies: ["SplitHView"]),
     ]
 )
