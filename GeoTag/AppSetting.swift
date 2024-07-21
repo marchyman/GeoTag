b99adba0742ceaa1cecb493ff05c798f4a409c64
Marco S Hyman <marc@snafu.org>
2024-07-20 17:27:08 -0700

    Remove all GeoTag defaults when UI testing

--- GeoTag/AppSetting.swift
+++ GeoTag/AppSetting.swift
@@ -30,22 +30,10 @@ enum AppSettings {
     static let updateFileModificationTimesKey = "UpdateFileModificationTimes"
     static let updateGPSTimestampsKey = "UpdateGPSTimestamps"
 
-    // reset all settings to their defaults.  Used to set the app to a known
-    // state for user interface testing.
+    // Remove all user defaults for the app. Used to set the app to a known
+    // state when user interface testing.
     static func resetSettings() {
-        let defaults = UserDefaults.standard
-        defaults.removeObject(forKey: addTagsKey)
-        defaults.removeObject(forKey: coordFormatKey)
-        defaults.removeObject(forKey: createSidecarFilesKey)
-        defaults.removeObject(forKey: disablePairedJpegsKey)
-        defaults.removeObject(forKey: doNotBackupKey)
-        defaults.removeObject(forKey: finderTagKey)
-        defaults.removeObject(forKey: hideInvalidImagesKey)
-        defaults.removeObject(forKey: imageTableConfigKey)
-        defaults.removeObject(forKey: savedBookmarkKey)
-        defaults.removeObject(forKey: splitHContentKey)
-        defaults.removeObject(forKey: splitVImageMapKey)
-        defaults.removeObject(forKey: updateFileModificationTimesKey)
-        defaults.removeObject(forKey: updateGPSTimestampsKey)
+        let domain = Bundle.main.bundleIdentifier!
+        UserDefaults.standard.removePersistentDomain(forName: domain)
     }
 }
