66d5cf4cc51d200b2f07918bdf2f1fd7a82bca68
Marco S Hyman <marc@snafu.org>
2024-11-04 10:33:02 -0800

    Add empty test to shut up xcode
    Xcode crashes when I tried to delete the test target.

---
+++ GeoTagTests/Empty.swift
@@ -0,0 +1,17 @@
+//
+// Copyright 2024 Marco S Hyman
+// See LICENSE file for info
+// https://www.snafu.org/
+//
+
+import Testing
+
+struct Test {
+
+    // needed so shut up Xcode -- was complaining about no tests but would
+    // crash if I tried to remove the test target
+    @Test func empty() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
