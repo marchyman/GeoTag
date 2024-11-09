a1a92e69c94d8e504546c14c4475cde04a0a9e56
Marco S Hyman <marc@snafu.org>
2024-11-08 17:38:37 -0800

    SplitVView swift-format changes

--- SplitVView/Tests/SplitVViewTests/SplitVViewTests.swift
+++ SplitVView/Tests/SplitVViewTests/SplitVViewTests.swift
@@ -1,12 +1,13 @@
 import XCTest
+
 @testable import SplitVView
 
 final class SplitVViewTests: XCTestCase {
-//    func testExample() throws {
-//        // XCTest Documentation
-//        // https://developer.apple.com/documentation/xctest
-//
-//        // Defining Test Cases and Test Methods
-//        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-//    }
+    //    func testExample() throws {
+    //        // XCTest Documentation
+    //        // https://developer.apple.com/documentation/xctest
+    //
+    //        // Defining Test Cases and Test Methods
+    //        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    //    }
 }
