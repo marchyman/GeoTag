0b530d3d4a613467635fb48b1780bc0eb199e77c
Marco S Hyman <marc@snafu.org>
2025-05-14 15:30:19 -0700

    fix the test target

---
+++ GeoTagTests/GeoTagTests.swift
@@ -0,0 +1,16 @@
+//
+//  GeoTagTests.swift
+//  GeoTagTests
+//
+//  Created by Marco S Hyman on 2025-05-14.
+//
+
+import Testing
+
+struct GeoTagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
