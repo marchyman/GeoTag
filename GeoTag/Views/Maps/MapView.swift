a45fd1e82afed14b625ed54ab2eec644cb08132b
Marco S Hyman <marc@snafu.org>
2026-02-20 15:46:39 -0800

    Show map pins. Recenter view when necessary.

--- GeoTag/Views/Maps/MapView.swift
+++ GeoTag/Views/Maps/MapView.swift
@@ -1,43 +1,57 @@
+import Coords
 import MapKit
 import SwiftUI
+import UDF
 
 struct MapView: View {
-    @State private var cameraPosition: MapCameraPosition = .automatic
-    @State private var cameraDistance: Double = 0
-    @State private var mapRect: MKMapRect?
-    @State private var camera: MapCamera?
-    @State private var mapStyleName: MapStyleName = .standard
+    @Environment(Store<GeoTagState, GeoTagEvent>.self) var store
 
     @AppStorage(Self.initialMapLatitudeKey) var initialMapLatitude = 37.7244
     @AppStorage(Self.initialMapLongitudeKey) var initialMapLongitude = -122.4381
     @AppStorage(Self.initialMapDistanceKey) var initialMapDistance = 50_000.0
     @AppStorage(Self.savedMapStyleKey) var savedMapStyle = MapStyleName.standard.rawValue
 
+    @State private var cameraPosition: MapCameraPosition = .automatic
+    @State private var cameraDistance: Double = 0
+    @State private var mapRect: MKMapRect?
+    @State private var camera: MapCamera?
+    @State private var mapStyleName: MapStyleName = .standard
+    @State private var mainPin: Coords?
+    @State private var allPins: [Coords] = []
+
     var body: some View {
         MapReader { mapProxy in
             Map(position: $cameraPosition) {
+                if let mainPin {
+                    Annotation("main pin",
+                               coordinate: mainPin,
+                               anchor: .bottom) {
+                        Image(.pin)
+                    }
+                    .annotationTitles(.hidden)
+                }
                 //
             }
-            //.mapStyle(mapStyleName.mapStyle)
+            .mapStyle(mapStyleName.mapStyle())
             .mapControls {
                 MapCompass()
                 MapPitchToggle()
                 MapScaleView()
                 MapZoomStepper()
             }
-            // .simultaneousGesture(SpatialTapGesture().onEnded { position in
-            //     mapFocus.wrappedValue = nil  // get rid of any search views
-            //     if let loc = mapProxy.convert(position.location, from: .local) {
-            //         updatePins(loc)
-            //     }
-            // })
-            // // needed for macOS 16 and earlier
-            // .gesture(zoomOut)
-            // .onTapGesture(count: 2) { position in
-            //     if let coords = mapProxy.convert(position, from: .local) {
-            //         zoom(around: coords)
-            //     }
-            // }
+            .simultaneousGesture(SpatialTapGesture().onEnded { position in
+                // mapFocus.wrappedValue = nil  // get rid of any search views
+                if let loc = mapProxy.convert(position.location, from: .local) {
+                    // TODO
+                    // updatePins(loc)
+                    print("loc changed to \(loc)")
+                }
+            })
+            .onTapGesture(count: 2) { position in
+                if let coords = mapProxy.convert(position, from: .local) {
+                    zoom(around: coords)
+                }
+            }
             .onMapCameraChange(frequency: .onEnd) { context in
                 camera = context.camera
                 if let distance = camera?.distance {
@@ -70,12 +84,55 @@ struct MapView: View {
                     latitude: initialMapLatitude,
                     longitude: initialMapLongitude)
                 cameraDistance = initialMapDistance
-                cameraPosition =
-                    .camera(
-                        .init(
-                            centerCoordinate: center,
-                            distance: cameraDistance))
+                setCameraPosition(to: center)
                 mapStyleName = .init(rawValue: savedMapStyle) ?? .standard
+            }
+            .task(id: store.mostSelected) {
+                print("mostSelected changed")
+                if let id = store.mostSelected {
+                    mainPin = store[id].metadata.location
+                    allPins = store.selection.compactMap {
+                        store[$0].metadata.location
+                    }
+                    recenter(to: mainPin)
+                } else {
+                    mainPin = nil
+                    allPins.removeAll()
+                }
+            }
+        }
+    }
+}
+
+// Map positioning helper functions
+
+extension MapView {
+    // Set the camera position
+    func setCameraPosition(to coords: Coords) {
+        cameraPosition = .camera(.init(centerCoordinate: coords,
+                                       distance: cameraDistance))
+    }
+
+    // recenter map if the given coords are not in view
+    func recenter(to coords: Coords?) {
+        if let coords, let rect = mapRect {
+            if !rect.contains(MKMapPoint(coords)) {
+                setCameraPosition(to: coords)
+            }
+        }
+    }
+
+    // Zoom in or out by a factor of two
+    private func zoom(around coords: CLLocationCoordinate2D, out: Bool = false) {
+        if let camera {
+            let distance =
+                out
+                ? camera.distance * 2
+                : camera.distance / 2
+            withAnimation(.easeInOut) {
+                cameraPosition =
+                    .camera(.init(centerCoordinate: coords,
+                                  distance: distance))
             }
         }
     }
