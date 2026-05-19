48ca70bca68eee6c7076cb76326807567cfe26aa
Marco S Hyman <marc@snafu.org>
2026-05-18 20:42:23 -0700

    GeoTag 6 App Store Screenshots

--- App Store Screenshots/screenshots.sh
+++ App Store Screenshots/screenshots.sh
@@ -3,9 +3,10 @@
 # Convert png screenshots to jpg images suitable for upload to app store
 
 set -e
+set -x
 
 for file in *.png; do
-    magick -resize 70%x70% "${file}" \
+    magick "${file}" -resize 70%x70% \
         -gravity center \
         -background "rgb(210,210,210)" \
         -extent 2560x1600 \
